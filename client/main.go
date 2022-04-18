a9368b49aeacc955d1ae83b6a3fdb9145151a427
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-04-18 08:12:48 -0400

    Initial commit

---
+++ client/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	pbExample "github.com/cwxstat/grpc-gateway-template/proto"
+	structpb "google.golang.org/protobuf/types/known/structpb"
+
+	"context"
+	"flag"
+	"log"
+	"time"
+
+)
+
+const (
+	defaultName = "world"
+)
+
+var (
+	addr = flag.String("addr", "0.0.0.0:11000", "the address to connect to")
+	name = flag.String("name", defaultName, "Name to greet")
+)
+
+func getCert() (credentials.TransportCredentials, error) {
+	creds, err := credentials.NewClientTLSFromFile("./certs/proto-certs.pem", 
+	"localhost")
+	if err != nil {
+		return nil, err
+	}
+	return creds, nil
+}
+
+func main() {
+	flag.Parse()
+	// Set up a connection to the server.
+
+
+	creds, err := getCert()
+	if err != nil {
+		log.Fatalf("could not find certs")
+	}
+	conn, err := grpc.Dial(*addr,
+		grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pbExample.NewUserServiceClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+    
+
+
+
+	r, err := c.AddUser(ctx, &pbExample.AddUserRequest{
+		Email:    "mchirico@gmail.com",
+		Metadata: &structpb.Struct{},
+	})
+
+
+	if err != nil {
+		log.Fatalf("could not addUser: %v", err)
+	}
+	log.Printf("Create time: %s", r.CreateTime)
+
+}
